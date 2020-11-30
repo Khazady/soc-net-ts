4244c79803c5b8c4a5adfb5d3397184e80353c81
Khazady <seakingtourist@gmail.com>
2020-12-01 01:16:33 +0300

    Updates

---
+++ precache-manifest.ee6a29ca30fb1ef2266a88ce7c731f01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1c9f464bd5def1ba0ba37c3d57a00b",
+    "url": "/soc-net-ts/index.html"
+  },
+  {
+    "revision": "1b4e678e8929a3c280e2",
+    "url": "/soc-net-ts/static/css/main.45dadf9a.chunk.css"
+  },
+  {
+    "revision": "b381fff99d6a41de8d1e",
+    "url": "/soc-net-ts/static/js/2.003452f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/soc-net-ts/static/js/2.003452f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4e678e8929a3c280e2",
+    "url": "/soc-net-ts/static/js/main.cd053583.chunk.js"
+  },
+  {
+    "revision": "6d22942be4bfae6dac2c",
+    "url": "/soc-net-ts/static/js/runtime-main.f74142b4.js"
+  },
+  {
+    "revision": "9d32f7cadeb9c987307fc28b62e33893",
+    "url": "/soc-net-ts/static/media/lookingForAJob.9d32f7ca.jpg"
+  },
+  {
+    "revision": "a98dd7aa8fcdbdb257b8b3b54d069e31",
+    "url": "/soc-net-ts/static/media/notLookingForAJob.a98dd7aa.jpg"
+  },
+  {
+    "revision": "dacce764e9def8e6212d80245d87a005",
+    "url": "/soc-net-ts/static/media/preloader.dacce764.svg"
+  }
+]);
