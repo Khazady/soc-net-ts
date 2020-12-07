64bd570be8fe097fbd33a6f26c5db18a031d22e2
Khazady <seakingtourist@gmail.com>
2020-12-07 21:42:57 +0300

    added open graph meta tags for links in socials, changed BrowserRouter to HashRouter for correct work github pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route, withRouter} from 'react-router-dom';
+import {HashRouter, Route, withRouter} from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer';
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from './components/Login/Login';
@@ -73,9 +73,9 @@ let AppContainer: any = compose(
 
 //чтобы работали тесты App
 export const MainApp = () => (
-  <BrowserRouter>
+  <HashRouter>
       <Provider store={store}>
           <AppContainer/>
       </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
